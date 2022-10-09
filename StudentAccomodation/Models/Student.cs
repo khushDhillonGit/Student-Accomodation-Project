ba12a011ef83699e987b79b14899ac4b33d968af
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-10-09 19:38:24 -0400

    Created student model class to store information about students

---
+++ StudentAccomodation/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAccomodation.Models
+{
+    public class Student
+    {
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string StudentEmail { get; set; }
+
+        //one student can only be in one house
+        public House? House { get; set; }
+    }
+}
