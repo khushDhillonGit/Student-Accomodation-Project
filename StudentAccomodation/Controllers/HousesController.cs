75592302fdd37d62c42cc6d480b593c47f8f04c1
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-10-09 20:23:58 -0400

    Created House controlled

---
+++ StudentAccomodation/Controllers/HousesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentAccomodation.Data;
+using StudentAccomodation.Models;
+
+namespace StudentAccomodation.Controllers
+{
+    public class HousesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HousesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Houses
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Houses.ToListAsync());
+        }
+
+        // GET: Houses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Houses == null)
+            {
+                return NotFound();
+            }
+
+            var house = await _context.Houses
+                .FirstOrDefaultAsync(m => m.HouseId == id);
+            if (house == null)
+            {
+                return NotFound();
+            }
+
+            return View(house);
+        }
+
+        // GET: Houses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Houses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HouseId,HouseName,MonthRent,HouseNumber,street,city,postalCode")] House house)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(house);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(house);
+        }
+
+        // GET: Houses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Houses == null)
+            {
+                return NotFound();
+            }
+
+            var house = await _context.Houses.FindAsync(id);
+            if (house == null)
+            {
+                return NotFound();
+            }
+            return View(house);
+        }
+
+        // POST: Houses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HouseId,HouseName,MonthRent,HouseNumber,street,city,postalCode")] House house)
+        {
+            if (id != house.HouseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(house);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HouseExists(house.HouseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(house);
+        }
+
+        // GET: Houses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Houses == null)
+            {
+                return NotFound();
+            }
+
+            var house = await _context.Houses
+                .FirstOrDefaultAsync(m => m.HouseId == id);
+            if (house == null)
+            {
+                return NotFound();
+            }
+
+            return View(house);
+        }
+
+        // POST: Houses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Houses == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Houses'  is null.");
+            }
+            var house = await _context.Houses.FindAsync(id);
+            if (house != null)
+            {
+                _context.Houses.Remove(house);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HouseExists(int id)
+        {
+          return _context.Houses.Any(e => e.HouseId == id);
+        }
+    }
+}
