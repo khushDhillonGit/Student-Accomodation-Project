783d111b81e1297966110880fd5c7cf154a5e53b
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-10-09 20:23:33 -0400

    connected azure database and did migrations to update the azure database from model classes

--- StudentAccomodation/Data/ApplicationDbContext.cs
+++ StudentAccomodation/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentAccomodation.Models;
 
 namespace StudentAccomodation.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
