2af46cb5c49bd0e1ebe12e8cf6693ab5a6f66e49
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-10-09 20:23:33 -0400

    connected azure database and did migrations to update the azure database from model classes

---
+++ StudentAccomodation/Data/Migrations/20221010000237_CreateInitalDatabase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentAccomodation.Data.Migrations
+{
+    public partial class CreateInitalDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    HouseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HouseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MonthRent = table.Column<int>(type: "int", nullable: false),
+                    HouseNumber = table.Column<int>(type: "int", nullable: false),
+                    street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postalCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.HouseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HouseId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Houses_HouseId",
+                        column: x => x.HouseId,
+                        principalTable: "Houses",
+                        principalColumn: "HouseId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_HouseId",
+                table: "Students",
+                column: "HouseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Houses");
+        }
+    }
+}
