64189d9831e56f0a9a5648117aaece7052c91030
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-11-04 00:19:02 -0400

    Added authorisation for registered users and index view allowed for anonymous users

--- StudentAccomodation/Controllers/StudentsController.cs
+++ StudentAccomodation/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using StudentAccomodation.Models;
 
 namespace StudentAccomodation.Controllers
 {
+    [Authorize]
     public class StudentsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,6 +22,7 @@ namespace StudentAccomodation.Controllers
         }
 
         // GET: Students
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Students.Include(s => s.House);
